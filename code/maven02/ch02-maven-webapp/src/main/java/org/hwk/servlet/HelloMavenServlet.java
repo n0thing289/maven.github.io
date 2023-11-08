e50572df3c4c2697ff0ce09e950e952a737a9104
n0thing289 <61171141+n0thing289@users.noreply.github.com>
2023-11-08 21:18:06 +0800

    maven

---
+++ code/maven02/ch02-maven-webapp/src/main/java/org/hwk/servlet/HelloMavenServlet.java
@@ -0,0 +1,16 @@
+package org.hwk.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({"/hello"})
+public class HelloMavenServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().print("你好Maven");
+    }
+}
---
+++ code/maven02/ch02-maven-webapp/target/ch02-maven-webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@page contentType="text/html;charset=UTF-8"%>
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="/webapp/hello">第一个maven构建的webapp项目</a>
+</body>
+</html>
